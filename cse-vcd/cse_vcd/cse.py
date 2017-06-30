4de7f621ff0ceb35c1de67e121126469a683fa7d
Paco Gómez <contact@pacogomez.com>
2017-06-29 20:59:20 -0500

    license

---
+++ cse-vcd/cse_vcd/cse.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+# container-service-extension
+# Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: BSD-2
---
+++ cse-vcd/cse_vcd/metadata.py
@@ -0,0 +1,3 @@
+# container-service-extension
+# Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: BSD-2
