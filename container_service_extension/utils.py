af66f7849ed5d1edccaae230538f051fbf85d384
Paco Gómez <contact@pacogomez.com>
2017-08-01 19:50:09 -0500

    fixed utils

--- container_service_extension/utils.py
+++ container_service_extension/utils.py
@@ -8,7 +8,7 @@ import ssl
 
 
 def hex_chunks(s):
-    return lambda s: [s[i: i+2] for i in range(0, len(s), 2)]
+    return [s[i: i+2] for i in range(0, len(s), 2)]
 
 
 def get_thumbprint(host, port):
