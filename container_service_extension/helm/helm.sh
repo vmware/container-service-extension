3c3682c37381493d2822cfd094185bb2aedc5726
auppunda <auppunda@gmail.com>
2017-09-05 10:05:51 -0700

    removed temp directory
    Signed-off-by: auppunda <auppunda@gmail.com>

--- container_service_extension/helm/helm.sh
+++ container_service_extension/helm/helm.sh
@@ -9,14 +9,14 @@ i=$1
 
 if [ "$i" == '-i' ]
 then
-	cd $(mktemp -d)
 
-	curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
+	curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > "get_helm.sh"
 
-	chmod 700 get_helm.sh
+	chmod 700 "get_helm.sh"
 
-	./get_helm.sh
+	./"get_helm.sh"
 	# clean up unneccesary files
+	rm "get_helm.sh"
 
 fi
 
