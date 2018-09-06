bb93b9e5444565746a472b3b2cc624262ad0b0ff
Andrew Ni <niandrew7@gmail.com>
2018-09-06 16:27:49 -0700

    Comment out problematic lines in ubuntu customization script
    `apt-get -q dist-upgrade -y` will (most of the time) result in
    a grub update that presents a selection menu, which will hang
    and result in failed customization. These commands are not
    vital to CSE functionality, but will be looked into further.
    
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- scripts/cust-ubuntu-16.04.sh
+++ scripts/cust-ubuntu-16.04.sh
@@ -61,8 +61,12 @@ systemctl disable nfs-kernel-server.service
 ### common
 echo 'upgrading the system'
 # apt-mark hold open-vm-tools
-apt-get -q dist-upgrade -y
-apt-get -q autoremove -y
+
+### this line results in a grub update that presents a selection menu,
+### which hangs and results in failed customization. 
+### This is a temporary change that will be fixed soon
+# apt-get -q dist-upgrade -y
+# apt-get -q autoremove -y
 
 echo -n > /etc/machine-id
 sync
