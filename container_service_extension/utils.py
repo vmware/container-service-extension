89bfe9a73dfc3b609f7819836150ed98553b1258
Andrew Ni <niandrew7@gmail.com>
2018-10-18 15:50:16 -0700

    Refactor config validation and cse check
    Replaced get_config with a safer get_validated_config method.
    We now check that all properties and value types in the
    config file are valid. AMQP connection and vCD/VCs are validated.
    Any config file errors will be captured
    here and raise relevant exceptions. This function guarantees
    config file validity and safety.
    
    Changed `check_cse()` to `check_cse_installation()`. This
    function now directly takes a config dictionary, and checks that
    CSE is registered to vCD as an extension, and that catalogs and
    templates exist. Raises exception if installation is incorrect.
    
    `cse check` command default behavior now only validates config
    file. Added the `--check-install` flag. When set, `cse check`
    also runs check_cse_installation().
     `--template` option now has no effect unless
    `--check-install` is set.
    
    Refactored some code into general use functions and moved
    them to utils.py. (bool_to_msg(), get_sha256(),
    check_keys_and_value_types(), check_python_version(),
    check_file_permissions()). config.py should only handle:
    cse sample, config file, and cse installation.
    
    Created docstrings for functions and made improvements to
    existing docstrings.
    
    Tested CSE installation!
    
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- container_service_extension/utils.py
+++ container_service_extension/utils.py
@@ -9,6 +9,7 @@ import os
 import random
 import socket
 import ssl
+import stat
 import string
 import sys
 from urllib.parse import urlparse
@@ -16,14 +17,154 @@ from urllib.parse import urlparse
 from cachetools import LRUCache
 from pyvcloud.vcd.client import BasicLoginCredentials
 from pyvcloud.vcd.client import Client
+from pyvcloud.vcd.exceptions import EntityNotFoundException
 from pyvcloud.vcd.platform import Platform
 from pyvcloud.vcd.vapp import VApp
 from pyvcloud.vcd.vm import VM
 from vsphere_guest_run.vsphere import VSphere
 
 cache = LRUCache(maxsize=1024)
-
 LOGGER = logging.getLogger('cse.utils')
+
+# used for registering CSE to vCD
+CSE_EXT_NAME = 'cse'
+CSE_EXT_NAMESPACE = 'cse'
+
+# chunk size in bytes for file reading
+BUF_SIZE = 65536
+
+_type_to_string = {
+    str: 'string',
+    int: 'number',
+    bool: 'true/false',
+    dict: 'mapping',
+    list: 'sequence',
+}
+
+
+def catalog_exists(org, catalog_name):
+    try:
+        org.get_catalog(catalog_name)
+        return True
+    except EntityNotFoundException:
+        return False
+
+
+def catalog_item_exists(org, catalog_name, catalog_item):
+    try:
+        org.get_catalog_item(catalog_name, catalog_item)
+        return True
+    except EntityNotFoundException:
+        return False
+
+
+def bool_to_msg(value):
+    if value:
+        return 'success'
+    return 'fail'
+
+
+def get_sha256(filepath):
+    """Gets sha256 hash of file as a string.
+
+    :param str filepath: path to file.
+
+    :return: sha256 string for the file.
+
+    :rtype: str
+    """
+    sha256 = hashlib.sha256()
+    with open(filepath, 'rb') as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+            sha256.update(data)
+    return sha256.hexdigest()
+
+
+def check_keys_and_value_types(dikt, ref_dict, location='dictionary'):
+    """Compares a dictionary with a reference dictionary to ensure that
+    all keys and value types are the same.
+
+    :param dict dikt: the dictionary to check for validity
+    :param dict ref_dict: the dictionary to check against
+    :param str location: where this check is taking place, so error messages
+        can be more descriptive.
+
+    :raises KeyError: if @dikt has missing or invalid keys
+    :raises ValueError: if the value of a property in @dikt does not match with
+        the value of the same property in @ref_dict
+    """
+    ref_keys = set(ref_dict.keys())
+    keys = set(dikt.keys())
+
+    missing_keys = ref_keys - keys
+    invalid_keys = keys - ref_keys
+
+    if missing_keys:
+        click.secho(f"Missing keys in {location}: {missing_keys}", fg='red')
+    if invalid_keys:
+        click.secho(f"Invalid keys in {location}: {invalid_keys}", fg='red')
+
+    bad_value = False
+    for k in ref_keys:
+        if k not in keys:
+            continue
+        value_type = type(ref_dict[k])
+        if not isinstance(dikt[k], value_type):
+            click.secho(f"Value type for {location} property '{k}' should "
+                        f"be '{_type_to_string[value_type]}'", fg='red')
+            bad_value = True
+
+    if missing_keys or invalid_keys:
+        raise KeyError(f"Missing and/or invalid key in {location}")
+    if bad_value:
+        raise ValueError(f"Incorrect type for property value(s) in {location}")
+
+
+def check_python_version():
+    """Ensures that user's Python version >= 3.6.
+
+    :raises Exception: if user's Python version < 3.6.
+    """
+    click.echo(f"Required Python version: >= 3.6\nInstalled Python version: "
+               f"{sys.version_info.major}.{sys.version_info.minor}."
+               f"{sys.version_info.micro}")
+    if not sys.version_info.major >= 3 and sys.version_info.minor >= 6:
+        raise Exception('Python version is not up to date')
+
+
+def check_file_permissions(filename):
+    """Ensures that the file only has rw permission for Owner, and no
+    permissions for anyone else.
+
+    :param str filename: path to file.
+
+    :raises Exception: if file has 'x' permissions for Owner or 'rwx'
+        permissions for 'Others' or 'Group'.
+    """
+    err_msgs = []
+    file_mode = os.stat(filename).st_mode
+    if file_mode & stat.S_IXUSR:
+        msg = f"Remove execute permission of the Owner for the file {filename}"
+        click.secho(msg, fg='red')
+        err_msgs.append(msg)
+    if file_mode & stat.S_IROTH or file_mode & stat.S_IWOTH \
+            or file_mode & stat.S_IXOTH:
+        msg = f"Remove read, write and execute permissions of Others for " \
+              f"the file {filename}"
+        click.secho(msg, fg='red')
+        err_msgs.append(msg)
+    if file_mode & stat.S_IRGRP or file_mode & stat.S_IWGRP \
+            or file_mode & stat.S_IXGRP:
+        msg = f"Remove read, write and execute permissions of Group for the " \
+              f"file {filename}"
+        click.secho(msg, fg='red')
+        err_msgs.append(msg)
+
+    if err_msgs:
+        raise IOError(err_msgs)
 
 
 def get_data_file(filename):
