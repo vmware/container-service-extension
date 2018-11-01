fc69000c9d47f99c0a9c3fac35756ee87f38de35
Andrew Ni <niandrew7@gmail.com>
2018-11-01 15:27:03 -0700

    Fix static data file installation
    setup.cfg had unintuitive placement of static data files
    (such as scripts, license, notice). Without a virtual
    environment active, these scripts would be installed to
    somewhere like `usr/local/bin/Python3.7/`. With a virtual
    environment active, these scripts would be installed to
    the virtual environment's home directory, somewhere like
    `~/.virtualenvs/myvenv/`. To standardize this behavior,
    data files are now installed to the Python environment's
    `site-packages` folder in the `cse_scripts` directory.
    
    Updated get_data_file() to use pathlib. No longer checks for
    potential 'cse' subdirectory. Now only checks for potential
    'scripts' subdirectory.
    
    Tested get_data_file(), works as expected independent of virtualenv
    
    Fixes vmware/container-service-extension#145
    
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- setup.py
+++ setup.py
@@ -5,9 +5,28 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
 from setuptools import setup
+import pathlib
+
+osl = 'open_source_license_container-service-extension_1.2.0_GA.txt'
+cse_scripts_dir = '/container_service_extension_scripts'
 
 setup(
     setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
-    package_data = {'':['swagger/swagger.yaml']},
+    package_data={'': ['swagger/swagger.yaml']},
+    packages=['container-service-extension'],
+    data_files=[
+        (str(pathlib.Path(cse_scripts_dir)), ['scripts/init-photon-v2.sh',
+                                              'scripts/cust-photon-v2.sh',
+                                              'scripts/mstr-photon-v2.sh',
+                                              'scripts/node-photon-v2.sh',
+                                              'scripts/init-ubuntu-16.04.sh',
+                                              'scripts/cust-ubuntu-16.04.sh',
+                                              'scripts/mstr-ubuntu-16.04.sh',
+                                              'scripts/node-ubuntu-16.04.sh',
+                                              'scripts/nfsd-ubuntu-16.04.sh']),
+        (str(pathlib.Path('.')), ['LICENSE.txt',
+                                  'NOTICE.txt',
+                                  osl]),
+    ],
     pbr=True,
 )
