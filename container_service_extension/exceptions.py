fbdf459e3bacb9bf78144dfe901e1d3bf9207ad7
Sakthi Sundaram <sakthis@vmware.com>
2018-11-06 11:51:31 -0800

    [VCDA-809] Exception Handling Implementation For Create Cluster
    Signed-off-by: Sakthi Sundaram <sakthis@vmware.com>

--- container_service_extension/exceptions.py
+++ container_service_extension/exceptions.py
@@ -4,39 +4,39 @@
 from pyvcloud.vcd.exceptions import VcdException
 
 
-class CseServerException(VcdException):
+class CseServerError(VcdException):
     """Base class for cse server side operation related exceptions"""
 
 
-class ClusterOperationException(CseServerException):
+class ClusterOperationError(CseServerError):
     """Base class for all cluster operation related exceptions"""
 
 
-class CreateVAppException(ClusterOperationException):
-    """Raised when any error happens while creating Vapp"""
+class ClusterAlreadyExistsError(ClusterOperationError):
+    """Raised when creating a cluster that already exists"""
 
 
-class ClusterInitializationException(ClusterOperationException):
-    """Raised when any error happens while cluster initialization"""
-
-
-class NodeOperationException(ClusterOperationException):
-    """Base class for all node operation related exceptions"""
-
-
-class ClusterJoinException(ClusterOperationException):
+class ClusterJoiningError(ClusterOperationError):
     """ Raised when any error happens while cluster join operation"""
 
 
-class CreateMasterNodeException(NodeOperationException):
+class ClusterInitializationError(ClusterOperationError):
+    """Raised when any error happens while cluster initialization"""
+
+
+class NodeOperationError(ClusterOperationError):
+    """Base class for all node operation related exceptions"""
+
+
+class MasterNodeCreationError(NodeOperationError):
     """Raised when any error happens while adding master node"""
 
 
-class CreateWorkerNodeException(NodeOperationException):
+class WorkerNodeCreationError(NodeOperationError):
     """Raised when any error happens while adding worker node"""
 
 
-class CreateNFSNodeException(NodeOperationException):
+class NFSNodeCreationError(NodeOperationError):
     """Raised when any error happens while adding NFS node"""
 
 
