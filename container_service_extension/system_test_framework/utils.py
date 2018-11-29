631c617fb0c5e075fead26b93ea87b4ccdd3cf8b
Andrew Ni <niandrew7@gmail.com>
2018-11-28 18:53:01 -0800

    Set up scripts to use for testing
    get_data_file() now searches cwd first for script files.
    This enables us to test script customization, as well
    as other users to modify our scripts, and then run them,
    just by using `cse install` command while scripts are in cwd.
    
    Created a scripts folder inside 'system_tests' directory.
    This scripts folder holds the actual customization scripts as
    static data files. The default customization scripts here are
    empty, which saves a lot of testing time, since customization
    does not have to be tested several times.
    
    When customization does need to be tested, we can use
    prepare_customization_scripts() in system tests utils, which
    will copy the static customization scripts into the active
    blanks. teardown method that triggers after each test will
    restore this scripts directory to the default state.
    
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- container_service_extension/system_test_framework/utils.py
+++ container_service_extension/system_test_framework/utils.py
@@ -13,8 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import yaml
+import pathlib
 
+import yaml
+from vcd_cli.utils import to_dict
+
+# from container_service_extension.system_test_framework.environment import SCRIPTS_DIR, ACTIVE_PHOTON_CUST_SCRIPT, ACTIVE_UBUNTU_CUST_SCRIPT, 
+import container_service_extension.system_test_framework.environment as env
 
 def write_dict_to_file_as_yaml(input_dict, output_file_name):
     """Write a dictionary as yaml to a file.
@@ -25,3 +30,37 @@ def write_dict_to_file_as_yaml(input_dict, output_file_name):
     config_yaml = yaml.safe_dump(input_dict, default_flow_style=False)
     with open(output_file_name, 'w') as output_file:
         output_file.write(config_yaml)
+
+
+def prepare_customization_scripts():
+    """Copies real customization scripts to the active customization scripts.
+    Copy 'CUST-PHOTON.sh' to 'cust-photon-v2.sh'
+    Copy 'CUST-UBUNTU.sh' to 'cust-ubuntu-16.04.sh'
+
+    :raises FileNotFoundError: if script files cannot be found.
+    """
+
+    scripts_filepaths = {
+        f"{env.SCRIPTS_DIR}/{env.STATIC_PHOTON_CUST_SCRIPT}":
+            f"{env.SCRIPTS_DIR}/{env.STATIC_PHOTON_CUST_SCRIPT}",
+        f"{env.SCRIPTS_DIR}/{env.STATIC_UBUNTU_CUST_SCRIPT}":
+            f"{env.SCRIPTS_DIR}/{env.STATIC_UBUNTU_CUST_SCRIPT}",
+    }
+
+    for src, dst in scripts_filepaths:
+        pathlib.Path(dst).write_text(pathlib.Path(src).read_text())
+
+
+def restore_customizaton_scripts():
+    """Blanks out 'cust-photon-v2.sh' and 'cust-ubuntu-16.04.sh'.
+
+    :raises FileNotFoundError: if script files cannot be found.
+    """
+
+    scripts_paths = [
+        pathlib.Path(f"{env.SCRIPTS_DIR}/{env.ACTIVE_PHOTON_CUST_SCRIPT}"),
+        pathlib.Path(f"{env.SCRIPTS_DIR}/{env.ACTIVE_UBUNTU_CUST_SCRIPT}")
+    ]
+
+    for path in scripts_paths:
+        path.write_text('')
