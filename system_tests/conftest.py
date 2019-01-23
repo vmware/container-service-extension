4c18375a0e4ec5cd02530ec5c48d0a4804a868b4
Andrew Ni <niandrew7@gmail.com>
2019-01-23 13:58:12 -0800

    Added copyrights
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- system_tests/conftest.py
+++ system_tests/conftest.py
@@ -1,3 +1,7 @@
+# container-service-extension
+# Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+
 """
 conftest.py is used by pytest to automatically find shared fixtures.
 Fixtures defined here can be used without importing.
