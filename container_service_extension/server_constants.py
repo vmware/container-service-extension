9fdb2307e240304a20234b64f2a775e5b2b5209f
Harshneel More <moreh@vmware.com>
2019-01-29 10:47:45 -0800

    [VCDA-843] Update CSE install method to add rights for CSE/PKS authorization
    Signed-off-by: Harshneel More <moreh@vmware.com>

--- container_service_extension/server_constants.py
+++ container_service_extension/server_constants.py
@@ -11,6 +11,10 @@ CSE_SERVICE_NAMESPACE = 'cse'
 # DEPLOY RIGHTS
 # used by authorization framework to weed out unauthorized calls.
 CSE_NATIVE_DEPLOY_RIGHT_NAME = 'CSE NATIVE DEPLOY RIGHT'
-CSE_NATIVE_DEPLOY_RIGHT_DESCRIPTION = ''
+CSE_NATIVE_DEPLOY_RIGHT_DESCRIPTION = 'Right necessary to deploy kubernetes cluster via vCD apis in CSE'
+CSE_NATIVE_DEPLOY_RIGHT_CATEGORY = 'cseRight'
+CSE_NATIVE_DEPLOY_RIGHT_BUNDLE_KEY = 'cseBundleKey'
 CSE_PKS_DEPLOY_RIGHT_NAME = 'PKS DEPLOY RIGHT'
-CSE_PKS_DEPLOY_RIGHT_DESCRIPTION = ''
+CSE_PKS_DEPLOY_RIGHT_DESCRIPTION = 'Right necessary to deploy kubernetes cluster via vSphere apis in PKS'
+CSE_PKS_DEPLOY_RIGHT_CATEGORY = 'pksRight'
+CSE_PKS_DEPLOY_RIGHT_BUNDLE_KEY = 'pksBundleKey'
