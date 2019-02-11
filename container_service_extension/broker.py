56aba2fbfaebd0b9eef303c847962315d0dc5ca8
sakthisunda <sakthisunda@yahoo.com>
2019-02-11 15:15:03 -0800

    [VCDA-854] Enable/Disable ovdc command implementation (#216)
    * Client command ovdc enable/disable implementation
    * Changes done to support vcd cse ovdc enable/disable. Includes client side changes to support CLI and related backend changes to set and remove ovdc metadata for k8s with pks or vcd as container
      provider and to allow only sys-admins to execute this command. Tested manually with all conditions on enable/disable with ovdc, org, pks-plans with container providers. Also executed as sys-admin(should be allowed) and non-sys-admins (should not allow). Flake8 compliant.

--- container_service_extension/broker.py
+++ container_service_extension/broker.py
@@ -42,6 +42,7 @@ from container_service_extension.exceptions import NFSNodeCreationError
 from container_service_extension.exceptions import NodeCreationError
 from container_service_extension.exceptions import WorkerNodeCreationError
 from container_service_extension.logger import SERVER_LOGGER as LOGGER
+from container_service_extension.ovdc_cache import OvdcCache
 from container_service_extension.server_constants import \
     CSE_NATIVE_DEPLOY_RIGHT_NAME
 from container_service_extension.utils import ACCEPTED
@@ -81,7 +82,7 @@ spinner = spinning_cursor()
 
 
 def rollback(func):
-    """Decorator to rollback on cluster and node creation failures.
+    """Decorate to rollback on cluster and node creation failures.
 
     :param func: reference to the original function that is decorated
 
@@ -792,6 +793,63 @@ class DefaultBroker(AbstractBroker, threading.Thread):
                 stack_trace=stack_trace)
         finally:
             self._disconnect_sys_admin()
+
+    @exception_handler
+    def enable_ovdc_for_kubernetes(self):
+        """Enable ovdc for k8-cluster deployment on given container provider.
+
+        :return: result object
+
+        :rtype: dict
+
+        :raises CseServerError: if the user is not system administrator.
+        """
+        result = dict()
+        self._connect_tenant()
+        if self.tenant_client.is_sysadmin():
+            ovdc_cache = OvdcCache(self.tenant_client)
+            task = ovdc_cache.set_ovdc_container_provider_metadata(
+                self.body['ovdc_name'],
+                ovdc_id=self.body.get('ovdc_id', None),
+                container_provider=self.body.get('container_provider', None),
+                pks_plans=self.body['pks_plans'],
+                org_name=self.body.get('org_name', None))
+            response_body = dict()
+            response_body['ovdc_name'] = self.body['ovdc_name']
+            response_body['task_href'] = task.get('href')
+            result['body'] = response_body
+            result['status_code'] = ACCEPTED
+            return result
+        else:
+            raise CseServerError("Unauthorized Operation")
+
+    @exception_handler
+    def ovdc_info_for_kubernetes(self):
+        """Info on ovdc for k8s deployment on the given container provider.
+
+        :return: result object
+
+        :rtype: dict
+
+        :raises CseServerError: if the user is not system administrator.
+        """
+        result = dict()
+        self._connect_tenant()
+        if self.tenant_client.is_sysadmin():
+            ovdc_cache = OvdcCache(self.tenant_client)
+            metadata = ovdc_cache.get_ovdc_container_provider_metadata(
+                self.body.get('ovdc_name', None),
+                ovdc_id=self.body.get('ovdc_id', None),
+                org_name=self.body.get('org_name', None))
+            # remove username, secret from sending to client
+            metadata.pop('username', None)
+            metadata.pop('secret', None)
+            result = dict()
+            result['status_code'] = OK
+            result['body'] = metadata
+            return result
+        else:
+            raise CseServerError("Unauthorized Operation")
 
     def node_rollback(self, node_list):
         """Rollback for node creation failure.
--- container_service_extension/ovdc_cache.py
+++ container_service_extension/ovdc_cache.py
@@ -3,7 +3,12 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
 from pyvcloud.vcd import utils
+from pyvcloud.vcd.client import MetadataDomain
+from pyvcloud.vcd.client import MetadataVisibility
+from pyvcloud.vcd.vdc import VDC
 
+
+from container_service_extension.logger import SERVER_LOGGER as LOGGER
 from container_service_extension.utils import get_org
 from container_service_extension.utils import get_vdc
 
@@ -47,6 +52,10 @@ class PvdcCacheStub(object):
 
 class OvdcCache(object):
 
+    __ovdc_metadata_keys = ['name', 'vc', 'rp_path', 'host', 'port',
+                            'uaac_port', 'pks_plans',
+                            'pks_compute_profile_name', 'container_provider']
+
     def __init__(self, client):
         """Construct the cache for ovdc.
 
@@ -56,10 +65,9 @@ class OvdcCache(object):
         self.client = client
         self.pvdc_cache = PvdcCacheStub()
 
-    def get_ovdc_backend_metadata(self, ovdc_name, org_name=None):
-        """Get ovdc metadata pertaining to the backend(vcd/pks).
-
-        The backend that this ovdc is dedicated to deploy K8 clusters on.
+    def get_ovdc_container_provider_metadata(self, ovdc_name,
+                                             ovdc_id=None, org_name=None):
+        """Get metadata of given ovdc, pertaining to the container provider.
 
         :param str ovdc_name: name of the ovdc
         :param str org_name: specific org to use if @org is not given.
@@ -72,53 +80,70 @@ class OvdcCache(object):
         :raises EntityNotFoundException: if the ovdc could not be found.
         """
         # Get pvdc and pks information from pvdc cache
-        ovdc = get_vdc(self.client, ovdc_name, org_name=org_name,
-                       is_admin_operation=True)
-        metadata = utils.metadata_to_dict(ovdc.get_all_metadata())
-        metadata['backend'] = metadata.get('backend', None)
+        if ovdc_id is None:
+            ovdc = get_vdc(self.client, ovdc_name, org_name=org_name,
+                           is_admin_operation=True)
+        else:
+            # TODO() - Implement this in pyvcloud
+            ovdc = self._get_vdc_by_id(ovdc_id)
 
-        if metadata['backend'] is None or metadata['backend'].lower() == 'vcd':
-            return {'backend': metadata['backend']}
+        all_metadata = utils.metadata_to_dict(ovdc.get_all_metadata())
 
-        pvdc_element = ovdc.resource.ProviderVdcReference
-        pvdc_id = pvdc_element.get('id')
-        pvdc_info = self.pvdc_cache.get_pvdc_info(pvdc_id)
-        pks_info = self.pvdc_cache.get_pks_info(ovdc.name, pvdc_info['vc'])
+        if 'container_provider' not in all_metadata:
+            container_provider = None
+        else:
+            container_provider = \
+                all_metadata['container_provider']
 
-        # Get ovdc metadata from vcd; copy the credentials from pvdc cache
-        metadata['rp_path'] = metadata['rp_path'].split(',')
-        metadata['pks_plans'] = metadata['pks_plans'].split(',')
-        metadata['username'] = pks_info['username']
-        metadata['secret'] = pks_info['secret']
+        if container_provider == 'pks':
+            # Filter out container provider metadata into a dict
+            metadata = {metadata_key: all_metadata[metadata_key]
+                        for metadata_key in self.__ovdc_metadata_keys}
+            pvdc_element = ovdc.resource.ProviderVdcReference
+            pvdc_id = pvdc_element.get('id')
+            pvdc_info = self.pvdc_cache.get_pvdc_info(pvdc_id)
+            pks_info = self.pvdc_cache.get_pks_info(ovdc.name, pvdc_info['vc'])
+
+            # Get ovdc metadata from vcd; copy the credentials from pvdc cache
+            metadata['rp_path'] = metadata['rp_path'].split(',')
+            metadata['pks_plans'] = metadata['pks_plans'].split(',')
+            metadata['username'] = pks_info['username']
+            metadata['secret'] = pks_info['secret']
+        else:
+            metadata = {'container_provider': container_provider}
+
         return metadata
 
-    def set_ovdc_backend_meta_data(self, ovdc_name, org_name=None,
-                                   backend=None,
-                                   pks_plans=''):
-        """Set the backing pvdc and pks information of a given oVdc.
+    def set_ovdc_container_provider_metadata(self, ovdc_name, ovdc_id=None,
+                                             org_name=None,
+                                             container_provider=None,
+                                             pks_plans=''):
+        """Set the container provider metadata of given ovdc.
 
         :param str ovdc_name: name of the ovdc
+        :param str ovdc_id: unique id of the ovdc
         :param str org_name: specific org to use if @org is not given.
             If None, uses currently logged-in org from @client.
-        :param str backend: name of back end for which this metadata is
-        required.
-        :param str pks_plans: pks plan for deployment. If backend is vcd
-            or None, plans are not used and not relevant to the context.
+        :param str container_provider: name of container provider for which
+            the ovdc is being enabled to deploy k8 clusters on.
+        :param str pks_plans: PKS plans for deployment. If container provider
+            is vCD or None, pks_plans are not applicable.
         """
         metadata = dict()
         org = get_org(self.client, org_name=org_name)
-        ovdc = get_vdc(self.client, ovdc_name, org=org,
-                       is_admin_operation=True)
-        if backend is None:
-            self._remove_metadata(ovdc,
-                                  keys=['name', 'vc', 'rp_path', 'host',
-                                        'port',
-                                        'uaac_port', 'pks_plans',
-                                        'pks_compute_profile_name'])
-            metadata['backend'] = ''
+        if ovdc_id is None:
+            ovdc = get_vdc(self.client, ovdc_name, org=org,
+                           is_admin_operation=True)
+            ovdc_id = utils.extract_id(ovdc.resource.get('id'))
+        else:
+            ovdc = self._get_vdc_by_id(ovdc_id)
+
+        if container_provider != 'pks':
+            LOGGER.debug(f'Remove metadata for ovdc:{ovdc_name}')
+            self._remove_metadata(ovdc, self.__ovdc_metadata_keys)
+            metadata['container_provider'] = container_provider or ''
         else:
             # Get resource pool
-            ovdc_id = ovdc.resource.get('id').split(':')[-1]
             resource_pool = f"{ovdc.name} ({ovdc_id})"
 
             # Get pvdc and pks information from pvdc cache
@@ -129,7 +154,6 @@ class OvdcCache(object):
             pks_info = self.pvdc_cache.get_pks_info(org_name, pvdc_info['vc'])
 
             # construct ovdc metadata
-
             metadata['name'] = pvdc_info['name']
             metadata['vc'] = pvdc_info['vc']
             metadata['rp_path'] = ','.join(
@@ -138,16 +162,25 @@ class OvdcCache(object):
             metadata['host'] = pks_info['host']
             metadata['port'] = pks_info['port']
             metadata['uaac_port'] = pks_info['uaac_port']
-            metadata['pks_plans'] = pks_plans
-            metadata['backend'] = backend
+            metadata['pks_plans'] = pks_plans or ''
+            metadata['container_provider'] = container_provider
             pks_compute_profile_name = f"{org_name}-{ovdc_name}-{ovdc_id}"
             metadata['pks_compute_profile_name'] = pks_compute_profile_name
 
         # set ovdc metadata into Vcd
-        ovdc.set_multiple_metadata(metadata)
+        LOGGER.debug(f"Setting below metadata on ovdc {ovdc_name}:{metadata}")
+        return ovdc.set_multiple_metadata(metadata, MetadataDomain.SYSTEM,
+                                          MetadataVisibility.PRIVATE)
 
     def _remove_metadata(self, ovdc, keys=[]):
         metadata = utils.metadata_to_dict(ovdc.get_all_metadata())
         for k in keys:
             if k in metadata:
-                ovdc.remove_metadata(k)
+                ovdc.remove_metadata(k, domain=MetadataDomain.SYSTEM)
+
+    def _get_vdc_by_id(self, vdc_id):
+        LOGGER.debug(f"Getting vdc by id:{vdc_id}")
+        admin_href = self.client.get_admin().get('href')
+        ovdc_href = f'{admin_href}vdc/{vdc_id}'
+        resource = self.client.get_resource(ovdc_href)
+        return VDC(self.client, resource=resource)
