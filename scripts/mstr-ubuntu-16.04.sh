d434aec00a23dbf99869830516655c38093baa16
Harshneel More <moreh@vmware.com>
2019-03-19 13:12:41 -0400

    [VCDA-903] Upgrade kubernetes templates from v1.10.11 to v1.13.4
    Signed-off-by: Harshneel More <moreh@vmware.com>

--- scripts/mstr-ubuntu-16.04.sh
+++ scripts/mstr-ubuntu-16.04.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 while [ `systemctl is-active docker` != 'active' ]; do echo 'waiting for docker'; sleep 5; done
-kubeadm init --kubernetes-version=v1.10.11 > /root/kubeadm-init.out
+kubeadm init --kubernetes-version=v1.13.4 > /root/kubeadm-init.out
 mkdir -p /root/.kube
 cp -f /etc/kubernetes/admin.conf /root/.kube/config
 chown $(id -u):$(id -g) /root/.kube/config
