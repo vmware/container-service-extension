4b168cc8d13837b7cb7e74fb1c295b17f8ae1410
sakthisunda <sakthisunda@yahoo.com>
2019-04-11 11:40:53 -0700

    Async keyword renamed; unsupported long data type merged to int type (#300)
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>

--- container_service_extension/pksclient/api/v1beta/cluster_api.py
+++ container_service_extension/pksclient/api/v1beta/cluster_api.py
@@ -126,7 +126,7 @@ class ClusterApi(object):
             files=local_var_files,
             response_type='Cluster',  # noqa: E501
             auth_settings=auth_settings,
-            async=params.get('async'),
+            is_async=params.get('async'),
             _return_http_data_only=params.get('_return_http_data_only'),
             _preload_content=params.get('_preload_content', True),
             _request_timeout=params.get('_request_timeout'),
@@ -219,7 +219,7 @@ class ClusterApi(object):
             files=local_var_files,
             response_type='Cluster',  # noqa: E501
             auth_settings=auth_settings,
-            async=params.get('async'),
+            is_async=params.get('async'),
             _return_http_data_only=params.get('_return_http_data_only'),
             _preload_content=params.get('_preload_content', True),
             _request_timeout=params.get('_request_timeout'),
