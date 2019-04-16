20c29920b99e85b93b36ac27f1faaad86ec76c24
Andrew Ni <niandrewdev@gmail.com>
2019-04-15 18:06:40 -0700

    [VCDA-975] Beautify CLI documentation (#296)
    - Required and optional parameters for different Kubernetes
    providers has been more clearly highlighted
    - Renamed 'container_provider' to 'k8s_provider'
    - Renamed 'vcd' to 'native'
    - Renamed 'pks' to 'ent-pks'
    - Renamed enablek8s, disablek8s, infok8s to
    	enable, disable, info
    - Revamped documentation for consistency, clarity, and usability
    - Make --org option consistent for list clusters
    - Fix error messages for cse run
    
    - Refactored the CtrProvType enum to be more consistent and have
    more intuitive behavior. Changed to 'K8sProvider', moved to
    server_constants.py, and added a str mixin.
    
    Refactored the code to use this enum instead of CtrProvType
    
    'vcd' -> 'native'
    'pks' -> 'ent-pks'
    
    
    - Change media_type for POST/PUT requests to application/json

--- container_service_extension/client/ovdc.py
+++ container_service_extension/client/ovdc.py
@@ -4,6 +4,8 @@
 
 from pyvcloud.vcd import utils
 
+from container_service_extension.server_constants import K8S_PROVIDER_KEY
+from container_service_extension.server_constants import K8sProviders
 from container_service_extension.utils import get_vdc
 from container_service_extension.utils import process_response
 
@@ -27,15 +29,15 @@ class Ovdc(object):
 
     def enable_ovdc_for_k8s(self,
                             ovdc_name,
-                            container_provider=None,
+                            k8s_provider=None,
                             pks_plan=None,
                             pks_cluster_domain=None,
                             org_name=None):
         """Enable ovdc for k8s for the given container provider.
 
         :param str ovdc_name: Name of the ovdc to be enabled
-        :param str container_provider: Name of the container provider
-        :param str pks_plan: pks plan
+        :param str k8s_provider: Name of the container provider
+        :param str pks_plan: PKS plan
         :param str pks_cluster_domain: Suffix of the domain name, which will be
          used to construct FQDN of the clusters.
         :param str org_name: Name of organization that belongs to ovdc_name
@@ -49,10 +51,11 @@ class Ovdc(object):
                        is_admin_operation=True)
         ovdc_id = utils.extract_id(ovdc.resource.get('id'))
         uri = f'{self._uri}/ovdc/{ovdc_id}/info'
+
         data = {
             'ovdc_id': ovdc_id,
             'ovdc_name': ovdc_name,
-            'container_provider': container_provider,
+            K8S_PROVIDER_KEY: k8s_provider,
             'pks_plans': pks_plan,
             'pks_cluster_domain': pks_cluster_domain,
             'org_name': org_name,
@@ -64,7 +67,7 @@ class Ovdc(object):
             uri,
             self.client._session,
             contents=data,
-            media_type=None,
+            media_type='application/json',
             accept_type='application/*+json')
         return process_response(response)
 
@@ -86,7 +89,7 @@ class Ovdc(object):
         data = {
             'ovdc_id': ovdc_id,
             'ovdc_name': ovdc_name,
-            'container_provider': None,
+            K8S_PROVIDER_KEY: K8sProviders.NONE,
             'pks_plans': None,
             'org_name': org_name,
             'disable': True
@@ -97,7 +100,7 @@ class Ovdc(object):
             uri,
             self.client._session,
             contents=data,
-            media_type=None,
+            media_type='application/json',
             accept_type='application/*+json')
         return process_response(response)
 
--- container_service_extension/cse.py
+++ container_service_extension/cse.py
@@ -118,7 +118,7 @@ def version(ctx):
     'output',
     required=False,
     default=None,
-    metavar='<output-file-name>',
+    metavar='OUTPUT_FILE_NAME',
     help="Name of the config file to dump the CSE configs to.")
 @click.option(
     '-p',
@@ -126,7 +126,7 @@ def version(ctx):
     'pks_output',
     required=False,
     default=None,
-    metavar='<pks-output-file-name>',
+    metavar='OUTPUT_FILE_NAME',
     help="Name of the PKS config file to dump the PKS configs to.")
 def sample(ctx, output, pks_output):
     """Generate sample CSE configuration."""
@@ -142,7 +142,7 @@ def sample(ctx, output, pks_output):
     '--config',
     'config',
     type=click.Path(exists=True),
-    metavar='<config-file>',
+    metavar='CONFIG_FILE_NAME',
     envvar='CSE_CONFIG',
     default='config.yaml',
     help='Config file to use.')
@@ -160,7 +160,7 @@ def sample(ctx, output, pks_output):
     'template',
     required=False,
     default='*',
-    metavar='<template>',
+    metavar='TEMPLATE_NAME',
     help="If '--check-install' flag is set, validate specified template. "
          "Default value of '*' means that all templates in config file"
          " will be validated.")
@@ -198,7 +198,7 @@ def check(ctx, config, check_install, template):
     '--config',
     'config',
     type=click.Path(exists=True),
-    metavar='<config-file>',
+    metavar='CONFIG_FILE_NAME',
     envvar='CSE_CONFIG',
     default='config.yaml',
     help='Config file to use.')
@@ -208,7 +208,7 @@ def check(ctx, config, check_install, template):
     'template',
     required=False,
     default='*',
-    metavar='<template>',
+    metavar='TEMPLATE_NAME',
     help="Install only the specified template. Default value of '*' means that"
          " all templates in config file will be installed.")
 @click.option(
@@ -270,7 +270,7 @@ def install(ctx, config, template, update, no_capture, ssh_key_file):
     '--config',
     'config',
     type=click.Path(exists=True),
-    metavar='<config-file>',
+    metavar='CONFIG_FILE_NAME',
     envvar='CSE_CONFIG',
     default='config.yaml',
     help='Config file to use.')
@@ -286,11 +286,8 @@ def run(ctx, config, skip_check):
     try:
         service = Service(config, should_check_config=not skip_check)
         service.run()
-    except (KeyError, TypeError):
-        click.secho(f"Config file '{config}' is invalid. Please "
-                    f"check the logs.", fg='red')
-    except (NotAcceptableException,
-            ValueError) as err:
+    except (NotAcceptableException, VcdException, ValueError, KeyError,
+            TypeError) as err:
         click.secho(str(err), fg='red')
     except AmqpConnectionError as err:
         click.secho(str(err), fg='red')
@@ -298,9 +295,6 @@ def run(ctx, config, skip_check):
     except requests.exceptions.ConnectionError:
         click.secho("Cannot connect to vCD host (check config file vCD host).",
                     fg='red')
-    except VcdException:
-        click.secho("vCD login failed (check config file vCD "
-                    "username/password).", fg='red')
     except vim.fault.InvalidLogin:
         click.secho("vCenter login failed (check config file vCenter "
                     "username/password).", fg='red')
