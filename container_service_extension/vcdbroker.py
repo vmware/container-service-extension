33e5bca1015abb4837854838378581a2556d8e16
malakars <malakars@vmware.com>
2019-05-21 12:04:36 -0700

    VCDA-1033: Proper message for missing network name error in cluster creation command (#343)
    * Proper error message for network name missing for creation of cluster command
    
    Signed-off-by: Sompa Malakar <malakars@vmware.com>
     Manually tested the command to ensure that proper error message is displayed when network name is missing from params, while creating clusters on vcd.

--- container_service_extension/vcdbroker.py
+++ container_service_extension/vcdbroker.py
@@ -364,6 +364,10 @@ class VcdBroker(AbstractBroker, threading.Thread):
         #  ClusterParams either as a param (or)
         #  read from instance variable (if needed only).
 
+        if not network_name:
+            raise CseServerError(f"Cluster cannot be created. Please provide"
+                             f" a valid value for org vDC network param.")
+
         LOGGER.debug(f"About to create cluster {cluster_name} on {vdc_name} "
                      f"with {node_count} nodes, sp={storage_profile}")
 
