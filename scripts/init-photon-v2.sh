722c427e335620fb23a339fa5b55f15ea7ef84a8
tironside <47457945+tironside@users.noreply.github.com>
2019-06-10 09:54:11 -0400

    Update init-photon-v2.sh

--- scripts/init-photon-v2.sh
+++ scripts/init-photon-v2.sh
@@ -6,6 +6,8 @@ chage -I -1 -m 0 -M -1 -E -1 root
 
 ### Comment below line if you want to enable root password access
 ### root password access is dangerous; ssh-key access is recommended
+### AuthorizedKeysFile is commented out in /etc/ssh/sshd_config add in fix
+sed -i '/AuthorizedKeysFile/s/#//g' /etc/ssh/sshd_config
 sed -i '/^PermitRootLogin yes/d' /etc/ssh/sshd_config
 systemctl reload sshd.service
 
