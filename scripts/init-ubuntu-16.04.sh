32d2e44c3f39c45bbcdcfe6fc6ea54a57541fe57
tironside <47457945+tironside@users.noreply.github.com>
2019-06-10 09:58:08 -0400

    Update init-ubuntu-16.04.sh
    ### AuthorizedKeysFile is commented out in /etc/ssh/sshd_config add in fix
    sed -i '/AuthorizedKeysFile/s/#//g' /etc/ssh/sshd_config

--- scripts/init-ubuntu-16.04.sh
+++ scripts/init-ubuntu-16.04.sh
@@ -3,7 +3,11 @@ set -e
 
 ### Uncomment if you want to enable root password access
 ### root password access is dangerous; ssh-key access is recommended
+
 # sed -i 's/prohibit-password/yes/' /etc/ssh/sshd_config
+
+### AuthorizedKeysFile is commented out in /etc/ssh/sshd_config add in fix
+sed -i '/AuthorizedKeysFile/s/#//g' /etc/ssh/sshd_config
 
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 apt remove -y cloud-init
