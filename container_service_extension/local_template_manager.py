dd9af5918ee47c7198ee710f432976a39e23ae5a
rocknes <aritra.iitkgp@gmail.com>
2019-07-19 14:48:05 -0700

    first cut
    Signed-off-by: rocknes <aritra.iitkgp@gmail.com>

---
+++ container_service_extension/local_template_manager.py
@@ -0,0 +1,60 @@
+# container-service-extension
+# Copyright (c) 2019 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+
+from pyvcloud.vcd.client import MetadataDomain
+from pyvcloud.vcd.client import MetadataValueType
+from pyvcloud.vcd.client import MetadataVisibility
+from pyvcloud.vcd.org import Org
+from pyvcloud.vcd.utils import metadata_to_dict
+
+
+def _filter_template_metadata(template_name, data):
+    filtered_data = {}
+    filtered_data['catalog_item_name'] = template_name
+    if data.get('admin_password'):
+        filtered_data['admin_password'] = data.get('admin_password')
+    if data.get('compute_policy'):
+        filtered_data['compute_policy'] = data.get('compute_policy')
+    if data.get('cpu'):
+        filtered_data['cpu'] = data.get('cpu')
+    if data.get('deprecated'):
+        filtered_data['deprecated'] = data.get('deprecated')
+    if data.get('description'):
+        filtered_data['description'] = data.get('description')
+    if data.get('mem'):
+        filtered_data['mem'] = data.get('mem')
+    if data.get('name'):
+        filtered_data['name'] = data.get('name')
+    if data.get('revision'):
+        filtered_data['revision'] = data.get('revision')
+
+    return filtered_data
+
+
+def get_all_metadata_on_catalog_item(client, catalog_name, catalog_item_name,
+                                     org_resource=None, org_name=None):
+    if org_resource is None:
+        org_resource = client.get_org_by_name(org_name=org_name)
+    org = Org(client, resource=org_resource)
+    md = org.get_all_metadata_from_catalog_item(
+        catalog_name=catalog_name,
+        item_name=catalog_item_name)
+    data = metadata_to_dict(md)
+    return _filter_template_metadata(catalog_item_name, data)
+
+
+def set_metadata_on_catalog_item(client, catalog_name, catalog_item_name,
+                                 data, org_resource=None, org_name=None):
+    if org_resource is None:
+        org_resource = client.get_org_by_name(org_name=org_name)
+    org = Org(client, resource=org_resource)
+
+    filtered_data = _filter_template_metadata(catalog_item_name, data)
+
+    return org.set_multiple_metadata_on_catalog_item(
+        catalog_name=catalog_name,
+        item_name=catalog_item_name,
+        key_value_dict=filtered_data,
+        domain=MetadataDomain.SYSTEM,
+        visibility=MetadataVisibility.PRIVATE)
