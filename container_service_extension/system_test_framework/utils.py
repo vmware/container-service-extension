916fedb3317f723414433717022f2e2a1507125f
Harshneel More <moreh@vmware.com>
2019-08-06 15:27:51 -0700

    [VCDA-1093] Add persona based tests for cluster create and list
    Signed-off-by: Harshneel More <moreh@vmware.com>

--- container_service_extension/system_test_framework/utils.py
+++ container_service_extension/system_test_framework/utils.py
@@ -14,8 +14,13 @@
 # limitations under the License.
 
 from pathlib import Path
+import re
 
+from vcd_cli.vcd import vcd
 import yaml
+
+import container_service_extension.system_test_framework.environment as env
+import container_service_extension.system_test_framework.utils as testutils
 
 
 def dict_to_yaml_file(dikt, filepath):
@@ -52,3 +57,50 @@ def get_temp_vapp_name(template_name):
 def format_command_info(cmd_root, cmd, exit_code, output):
     return f"\nCommand: [{cmd_root} {cmd}]\nExit Code: [{exit_code}]" \
            f"\nOutput Start===\n{output}===Output End"
+
+
+def execute_commands(cmd_list):
+    cmd_results = []
+    for action in cmd_list:
+        cmd = action.cmd
+        print(f"Running command [vcd {cmd}]")
+        expected_exit_code = action.exit_code
+        result = env.CLI_RUNNER.invoke(vcd, cmd.split(), input='y',
+                                       catch_exceptions=False)
+        assert result.exit_code == expected_exit_code, \
+            testutils.format_command_info(
+                'vcd', cmd, result.exit_code, result.output)
+
+        if action.validate_output_func is not None:
+            action.validate_output_func(result.output, action.test_user)
+
+        cmd_results.append(result)
+
+    return cmd_results
+
+
+def list_cluster_output_validator(output, runner_username):
+    """Test cse cluster list command output.
+
+    Validate cse cluster list command based on persona.
+
+    :param output: list of results from execution of cse commands
+    :param runner_username: persona used to run the command
+    """
+    def cluster_list_validator(output):
+        return len(re.findall('testcluster', output))
+
+    if runner_username == 'sys_admin':
+        # sys admin can see all the clusters
+        cluster_count = cluster_list_validator(output)
+        assert cluster_count == 3
+
+    if runner_username == 'org_admin':
+        # org admin can see all the clusters belonging to the org
+        cluster_count = cluster_list_validator(output)
+        assert cluster_count == 3
+
+    if runner_username == 'vapp_author':
+        # vapp author can only see clusters created by him
+        cluster_count = cluster_list_validator(output)
+        assert cluster_count == 1
