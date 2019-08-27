a78d792b6a5d64014b1d3417d895c49b718d9e32
rocknes <aritra.iitkgp@gmail.com>
2019-08-27 14:40:18 -0700

    Updating sample generator to pull template recipe from vmware repo by default.
    Signed-off-by: rocknes <aritra.iitkgp@gmail.com>

--- container_service_extension/sample_generator.py
+++ container_service_extension/sample_generator.py
@@ -126,7 +126,7 @@ SAMPLE_BROKER_CONFIG = {
         'storage_profile': '*',
         'default_template_name': 'photon_template',
         'default_template_revision': 0,
-        'remote_template_cookbook_url': 'https://raw.githubusercontent.com/rocknes/container-service-extension/remote_template/template.yaml', # noqa: E501
+        'remote_template_cookbook_url': 'https://raw.githubusercontent.com/vmware/container-service-extension-templates/master/template.yaml', # noqa: E501
     }
 }
 
