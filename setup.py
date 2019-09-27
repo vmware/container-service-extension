ccc75ca77586f25f38bd70852c29eecc2da5df2a
Sakthi Sundaram <sakthisunda@yahoo.com>
2019-09-27 12:19:04 -0700

    [VCDA-1138] Updated OSL file 2.5 GA release
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>

--- setup.py
+++ setup.py
@@ -8,12 +8,10 @@ from pathlib import Path
 
 from setuptools import setup
 
-osl = 'open_source_license_container-service-extension_2.0_GA.txt'
-cse_scripts_dir = '/container_service_extension_scripts'
+osl = 'open_source_license_container-service-extension_2.5_GA.txt'
 
 setup(
     setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
-    package_data={'': ['swagger/swagger.yaml']},
     packages=['container_service_extension'],
     data_files=[
         (str(Path('/')), ['LICENSE.txt', 'NOTICE.txt', osl]),
