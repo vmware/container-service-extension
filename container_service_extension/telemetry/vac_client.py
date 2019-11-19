c68b2e9a08d262d4dfef71bdff9424d94add8e77
Sakthi Sundaram <sakthisunda@yahoo.com>
2019-11-19 14:36:34 -0800

    VAC client for CSE
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>

---
+++ container_service_extension/telemetry/vac_client.py
@@ -0,0 +1,81 @@
+# container-service-extension
+# Copyright (c) 2019 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+
+import json
+
+import requests
+
+
+class VacClient(object):
+    """REST based client for Vmware Analytics Server.
+
+    Attributes:
+      base_url str: Vmware Analytics Cloud Url
+      collector_id str: unique id that is supplied by VAC for the source of
+      data.
+      instance_id str: an optional name that indicates who is using this
+      client. This may be for example: which vCD installation.
+
+    """
+
+    def __init__(self,
+                 base_url,
+                 collector_id,
+                 instance_id=None,
+                 verify_ssl=True,
+                 logger_instance=None,
+                 log_requests=False,
+                 log_headers=False,
+                 log_body=False):
+        self._base_url = base_url
+        self._collector_id = collector_id
+        self._instance_id = instance_id
+        self._verify_ssl = verify_ssl
+        self.LOGGER = logger_instance
+        self._log_requests = log_requests
+        self._log_headers = log_headers
+        self._log_body = log_body
+
+    def do_request(self, method, payload=None):
+        """Make a request to Vmware Analytics Server.
+
+        :param shared_constants.RequestMethod method: One of the HTTP verb
+        defined in the enum.
+        :param dict payload: JSON payload for the REST call.
+
+        :return: body of the response text (JSON) in form of a dictionary.
+
+        :rtype: dict
+
+        :raises HTTPError: if the underlying REST call fails.
+        """
+        url = f"{self._base_url}?_c={self._collector_id}"
+        if self._instance_id:
+            url += f"&_i={self._instance_id}"
+
+        response = requests.request(
+            method.value,
+            url,
+            json=payload,
+            verify=self._verify_ssl)
+
+        if self._log_requests:
+            self.LOGGER.debug(f"Request uri : {(method.value).upper()} {url}")
+            if self._log_headers:
+                self.LOGGER.debug("Request headers : "
+                                  f"{response.request.headers}")
+            if self._log_body and payload:
+                self.LOGGER.debug(f"Request body : {response.request.body}")
+
+        if self._log_requests:
+            self.LOGGER.debug(f"Response status code: {response.status_code}")
+            if self._log_headers:
+                self.LOGGER.debug(f"Response headers : {response.headers}")
+            if self._log_body:
+                self.LOGGER.debug(f"Response body : {response.text}")
+
+        response.raise_for_status()
+
+        if response.text:
+            return json.loads(response.text)
