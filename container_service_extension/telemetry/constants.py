67507d82952d29ef0ad44fd9cd98fcba3ee0c166
Sakthi Sundaram <sakthisunda@yahoo.com>
2019-12-04 15:40:07 -0800

    Add vac url, collector id in config at CSE server startup
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>

---
+++ container_service_extension/telemetry/constants.py
@@ -0,0 +1,11 @@
+# container-service-extension
+# Copyright (c) 2019 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+
+# End point of Vmware Analytics staging server
+# TODO() : This URL should reflect production server during release
+VAC_URL = "https://vcsa.vmware.com/ph-stg/api/hyper/send/"
+
+# Value of collector id that is required as part of HTTP request
+# to post sample data to analytics server
+COLLECTOR_ID = "CSE.2_6"
