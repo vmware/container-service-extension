3796549c84ec9a7f59b1f43ad487f5d8b3a51b33
rocknes <aritra.iitkgp@gmail.com>
2020-02-03 13:23:36 -0800

    Sort `cse template list` command's output
    Signed-off-by: rocknes <aritra.iitkgp@gmail.com>

--- container_service_extension/request_handlers/template_handler.py
+++ container_service_extension/request_handlers/template_handler.py
@@ -14,12 +14,14 @@ def template_list(request_data, tenant_auth_token, is_jwt_token):
     config = utils.get_server_runtime_config()
     templates = []
     for t in config['broker']['templates']:
+        is_default = (t[LocalTemplateKey.NAME], str(t[LocalTemplateKey.REVISION])) == (config['broker']['default_template_name'], str(config['broker']['default_template_revision'])) # noqa: E501
         templates.append({
             'name': t[LocalTemplateKey.NAME],
             'revision': t[LocalTemplateKey.REVISION],
-            'is_default': t[LocalTemplateKey.NAME] == config['broker']['default_template_name'] and str(t[LocalTemplateKey.REVISION]) == str(config['broker']['default_template_revision']), # noqa: E501
+            'is_default': 'Yes' if is_default else 'No',
             'catalog': config['broker']['catalog'],
             'catalog_item': t[LocalTemplateKey.CATALOG_ITEM_NAME],
             'description': t[LocalTemplateKey.DESCRIPTION].replace("\\n", ", ")
         })
-    return templates
+
+    return sorted(templates, key=lambda i: (i['name'], i['revision']), reverse=True)  # noqa: E501
