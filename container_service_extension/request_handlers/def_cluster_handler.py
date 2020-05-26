a770471b990a7855b8aa5894a7e4a934911dd652
Sahithi Ayloo <sahithi.ayloo@gmail.com>
2020-05-26 09:18:57 -0700

    Separate defined entity handler and implementation
    Signed-off-by: sahithi <sahithi.ayloo@gmail.com>

--- container_service_extension/request_handlers/def_cluster_handler.py
+++ container_service_extension/request_handlers/def_cluster_handler.py
@@ -1,4 +1,4 @@
-from pandas.core.resample import method
+
 
 import container_service_extension.exceptions as e
 from container_service_extension.def_modules.entity_svc import DefEntityService
@@ -147,6 +147,54 @@ def cluster_list(request_data, request_context: RequestContext):
 
     return result
 
+@record_user_action_telemetry(cse_operation=CseOperation.NODE_CREATE)
+def node_create(request_data, request_context: RequestContext):
+    """Request handler for node create operation.
+
+    Required data: cluster_name, network_name
+    Optional data and default values: org_name=None, ovdc_name=None,
+        num_nodes=1, num_cpu=None, mb_memory=None, storage_profile_name=None,
+        template_name=default, template_revision=default,
+        ssh_key=None, rollback=True, enable_nfs=False,
+
+    (data validation handled in broker)
+
+    :return: Dict
+    """
+    cluster_svc = DefClusterService(request_context)
+    return cluster_svc.create_nodes(data=request_data)
+
+
+@record_user_action_telemetry(cse_operation=CseOperation.NODE_DELETE)
+def node_delete(request_data, request_context: RequestContext):
+    """Request handler for node delete operation.
+
+    Required data: cluster_name, node_names_list
+    Optional data and default values: org_name=None, ovdc_name=None
+
+    (data validation handled in broker)
+
+    :return: Dict
+    """
+    cluster_svc = DefClusterService(request_context)
+    return cluster_svc.delete_nodes(data=request_data)
+
+
+@record_user_action_telemetry(cse_operation=CseOperation.NODE_INFO)
+def node_info(request_data, request_context: RequestContext):
+    """Request handler for node info operation.
+
+    Required data: cluster_name, node_name
+    Optional data and default values: org_name=None, ovdc_name=None
+
+    (data validation handled in broker)
+
+    :return: Dict
+    """
+    cluster_svc = DefClusterService(request_context)
+    return cluster_svc.get_node_info(data=request_data)
+
+
 
 _OPERATION_KEY = 'operation'
 
@@ -159,6 +207,9 @@ OPERATION_TO_METHOD = {
     CseOperation.CLUSTER_RESIZE: cluster_resize,
     CseOperation.CLUSTER_UPGRADE_PLAN: cluster_upgrade_plan,  # noqa: E501
     CseOperation.CLUSTER_UPGRADE: cluster_upgrade,
+    CseOperation.NODE_CREATE: node_create,
+    CseOperation.NODE_DELETE: node_delete,
+    CseOperation.NODE_INFO: node_info,
 }
 
 
