f758030acd2d5d73356a7737523c4760b01838ba
Sakthi Sundaram <sakthisunda@yahoo.com>
2020-06-22 08:43:50 -0700

    review comments addressed
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>

--- container_service_extension/client/cluster.py
+++ container_service_extension/client/cluster.py
@@ -27,9 +27,9 @@ class Cluster:
         :return: instance of version specific client side cluster
         """
         api_version = client.get_api_version()
-        if float(api_version) == float(vcd_client.ApiVersion.VERSION_33.value) or float(api_version) == float(vcd_client.ApiVersion.VERSION_34.value):   # noqa: E501
+        if float(api_version) < float(vcd_client.ApiVersion.VERSION_35.value):   # noqa: E501
             return LegacyNativeCluster(client)
-        elif float(api_version) == float(vcd_client.ApiVersion.VERSION_35):
+        elif float(api_version) >= float(vcd_client.ApiVersion.VERSION_35.value):  # noqa: E501
             if cluster_kind == ClusterKind.NATIVE or cluster_kind == ClusterKind.TKG_PLUS:  # noqa: E501
                 return NativeCluster(client)
             elif cluster_kind == ClusterKind.TKG:
--- container_service_extension/client/utils.py
+++ container_service_extension/client/utils.py
@@ -7,6 +7,7 @@ import requests
 from vcd_cli.profiles import Profiles
 
 from container_service_extension.client import system as syst
+from container_service_extension.shared_constants import CSE_SERVER_API_VERSION
 
 
 def cse_restore_session(ctx, vdc_required=False) -> None:
@@ -19,42 +20,37 @@ def cse_restore_session(ctx, vdc_required=False) -> None:
     :param bool vdc_required: is vdc required or not
     :return:
     """
-    # User initiated and expired logout overwrites the profiles.yaml in vcd_cli
-    # So,override the vcd_client by new client with server api version.
-    if type(ctx.obj) is dict and 'client' in ctx.obj and ctx.obj['client']:
-        _override_client(ctx)
-        return
+    # Always override the vcd_client by new client with CSE server api version.
+    if type(ctx.obj) is not dict or not ctx.obj.get('client'):
+        profiles = Profiles.load()
+        token = profiles.get('token')
+        if token is None or len(token) == 0:
+            raise Exception('Can\'t restore session, please login again.')
+        if not profiles.get('verify'):
+            if not profiles.get('disable_warnings'):
+                click.secho(
+                    'InsecureRequestWarning: '
+                    'Unverified HTTPS request is being made. '
+                    'Adding certificate verification is strongly '
+                    'advised.',
+                    fg='yellow',
+                    err=True)
+            requests.packages.urllib3.disable_warnings()
 
-    profiles = Profiles.load()
-    token = profiles.get('token')
-    if token is None or len(token) == 0:
-        raise Exception('Can\'t restore session, please login again.')
-    if not profiles.get('verify'):
-        if profiles.get('disable_warnings'):
-            pass
-        else:
-            click.secho(
-                'InsecureRequestWarning: '
-                'Unverified HTTPS request is being made. '
-                'Adding certificate verification is strongly '
-                'advised.',
-                fg='yellow',
-                err=True)
-        requests.packages.urllib3.disable_warnings()
+        client = Client(
+            profiles.get('host'),
+            api_version=profiles.get('api_version'),
+            verify_ssl_certs=profiles.get('verify'),
+            log_file='vcd.log',
+            log_requests=profiles.get('log_request'),
+            log_headers=profiles.get('log_header'),
+            log_bodies=profiles.get('log_body'))
+        client.rehydrate_from_token(
+            profiles.get('token'), profiles.get('is_jwt_token'))
 
-    client = Client(
-        profiles.get('host'),
-        api_version=profiles.get('api_version'),
-        verify_ssl_certs=profiles.get('verify'),
-        log_file='vcd.log',
-        log_requests=profiles.get('log_request'),
-        log_headers=profiles.get('log_header'),
-        log_bodies=profiles.get('log_body'))
-    client.rehydrate_from_token(
-        profiles.get('token'), profiles.get('is_jwt_token'))
+        ctx.obj = {}
+        ctx.obj['client'] = client
 
-    ctx.obj = {}
-    ctx.obj['client'] = client
     _override_client(ctx)
 
     if vdc_required:
@@ -66,23 +62,23 @@ def cse_restore_session(ctx, vdc_required=False) -> None:
 def _override_client(ctx) -> None:
     """Replace the vcd client in ctx.obj with new one.
 
-    New vcd client takes the server_api_version as api_version param.
-    Save profile also with 'server_api_version' for subsequent commands.
+    New vcd client takes the CSE server_api_version as api_version param.
+    Save profile also with 'cse_server_api_version' for subsequent commands.
 
     :param <click.core.Context> ctx: click context
     """
     profiles = Profiles.load()
-    server_api_version = profiles.get('server_api_version')
+    cse_server_api_version = profiles.get(CSE_SERVER_API_VERSION)
     # Get server_api_version; save it in profiles if doesn't exist
-    if not server_api_version:
+    if not cse_server_api_version:
         system = syst.System(ctx.obj['client'])
         sys_info = system.get_info()
-        server_api_version = sys_info.get('server_api_version')
-        profiles.set('server_api_version', server_api_version)
+        cse_server_api_version = sys_info.get(CSE_SERVER_API_VERSION)
+        profiles.set(CSE_SERVER_API_VERSION, cse_server_api_version)
         profiles.save()
     client = Client(
         profiles.get('host'),
-        api_version=server_api_version,
+        api_version=cse_server_api_version,
         verify_ssl_certs=profiles.get('verify'),
         log_file='vcd.log',
         log_requests=profiles.get('log_request'),
