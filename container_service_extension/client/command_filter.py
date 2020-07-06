71e0b7ddcde4cf85611e509bf237005a225553fb
Aniruddha Shamasundar <aniruddha.9794@gmail.com>
2020-07-05 18:58:51 -0700

    Fix client group filter
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>

--- container_service_extension/client/command_filter.py
+++ container_service_extension/client/command_filter.py
@@ -37,7 +37,7 @@ UNSUPPORTED_COMMANDS_BY_VERSION = {
         GroupKey.CLUSTER: ['apply']
     },
     vcd_client.ApiVersion.VERSION_35.value: {
-        GroupKey.CLUSTER: ['create']
+        GroupKey.CLUSTER: ['create'],
         GroupKey.OVDC: ['compute-policy']
     }
 }
