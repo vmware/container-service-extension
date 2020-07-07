7892b8ac2e2464722bf3d5c51c87cd4544fac633
sakthisunda <sakthisunda@yahoo.com>
2020-07-06 19:39:00 -0700

    [VCDA-1547] Implement vcd cse cluster apply for create cluster (#634)
    * [VCDA-1568] Exception-Handler For Defined Entity Services and Cluster Handler (#628)
    
    * Exception Handlers For Defined Entity Based Services and Cluster Handler
    
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>
    
    * review comments addressed
    
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>
    
    * cluster apply implementation
    
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>
    
    * review comments addressed
    
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>

--- container_service_extension/client/cluster.py
+++ container_service_extension/client/cluster.py
@@ -4,35 +4,37 @@
 
 import pyvcloud.vcd.client as vcd_client
 
-from container_service_extension.client.command_filter import ClusterKind
 from container_service_extension.client.def_entity_cluster import DefEntityCluster # noqa: E501
 from container_service_extension.client.legacy_native_cluster import LegacyNativeCluster  # noqa: E501
 from container_service_extension.client.native_cluster import NativeCluster  # noqa: E501
 from container_service_extension.client.tkg_cluster import TKGCluster
+from container_service_extension.def_.utils import ClusterEntityKind   # noqa: E501
 
 
 class Cluster:
     """Returns the cluster class as determined by API version."""
 
-    def __new__(cls, client: vcd_client, cluster_kind=None):
+    def __new__(cls, client: vcd_client, cluster_entity_kind=None):
         """Create the right cluster class for the negotiated API version.
 
         In case of ApiVersion.VERSION_35, return specific instance if the
         cluster kind is known up-front.
 
-        If the cluster kind is unknown, return instance of DefEntityCluster for
-        all common operations like get_cluster_info(), list_clusters()
+        If the cluster entity kind is unknown, return instance of
+        DefEntityCluster for all common operations like get_cluster_info(),
+        list_clusters()
 
         :param pyvcloud.vcd.client client: vcd client
+        :param dict cluster_config: cluster configuration
         :return: instance of version specific client side cluster
         """
         api_version = client.get_api_version()
         if float(api_version) < float(vcd_client.ApiVersion.VERSION_35.value):   # noqa: E501
             return LegacyNativeCluster(client)
         elif float(api_version) >= float(vcd_client.ApiVersion.VERSION_35.value):  # noqa: E501
-            if cluster_kind == ClusterKind.NATIVE or cluster_kind == ClusterKind.TKG_PLUS:  # noqa: E501
+            if cluster_entity_kind == ClusterEntityKind.NATIVE.value or cluster_entity_kind == ClusterEntityKind.TANZU_PLUS.value:  # noqa: E501
                 return NativeCluster(client)
-            elif cluster_kind == ClusterKind.TKG:
+            elif cluster_entity_kind == ClusterEntityKind.TKG.value:
                 return TKGCluster(client)
             else:
                 return DefEntityCluster(client)
