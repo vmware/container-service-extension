83b26a1b04eba0ddb1fa2391e5299aa2fc122ae4
Aniruddha Shamasundar <aniruddha.9794@gmail.com>
2020-07-29 12:17:08 -0700

    Prevent sending compute policy tag if sizing policy not given for cluster create
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>

--- container_service_extension/def_/cluster_service.py
+++ container_service_extension/def_/cluster_service.py
@@ -1539,11 +1539,11 @@ def add_nodes(sysadmin_client, num_nodes, node_type, org, vdc, vapp,
                     'hostname': name,
                     'password_auto': True,
                     'network': network_name,
-                    'ip_allocation_mode': 'pool',
-                    'placement_policy_href': config['placement_policy_hrefs'][template[LocalTemplateKey.KIND]]  # noqa: E501
+                    'ip_allocation_mode': 'pool'
                 }
                 if sizing_class_href:
                     spec['sizing_policy_href'] = sizing_class_href
+                    spec['placement_policy_href'] = config['placement_policy_hrefs'][template[LocalTemplateKey.KIND]]  # noqa: E501
                 if cust_script is not None:
                     spec['cust_script'] = cust_script
                 if storage_profile:
