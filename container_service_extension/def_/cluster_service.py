a52a62bf470480e9b76660c7ccb49c73709d9afe
Anirudh9794 <aniruddha.9794@gmail.com>
2020-08-03 12:18:27 -0700

    [VCDA-1681] cluster restriction with placement policies fixed (#675)
    * Placement policy addition before adding vms
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>
    
    * Cluster deployment with ovdc policies fixed
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>
    
    * Prevent sending compute policy tag if sizing policy not given for cluster create
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>
    
    * Address review comments
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>
    
    * remove line setting entity Type in cluster deployment
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>
    
    * changed pyvcloud requirements yaml
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>

--- container_service_extension/def_/cluster_service.py
+++ container_service_extension/def_/cluster_service.py
@@ -1786,6 +1786,7 @@ def add_nodes(sysadmin_client, num_nodes, node_type, org, vdc, vapp,
                 }
                 if sizing_class_href:
                     spec['sizing_policy_href'] = sizing_class_href
+                    spec['placement_policy_href'] = config['placement_policy_hrefs'][template[LocalTemplateKey.KIND]]  # noqa: E501
                 if cust_script is not None:
                     spec['cust_script'] = cust_script
                 if storage_profile:
@@ -1824,6 +1825,11 @@ def add_nodes(sysadmin_client, num_nodes, node_type, org, vdc, vapp,
             # TODO: get details of the exception to determine cause of failure,
             # e.g. not enough resources available.
             node_list = [entry.get('target_vm_name') for entry in specs]
+            if hasattr(err, 'vcd_error') and \
+                    "throwPolicyNotAvailableException" in err.vcd_error.get('stackTrace', ''):  # noqa: E501
+                raise e.NodeCreationError(node_list,
+                                          f"OVDC not enabled for {template[LocalTemplateKey.KIND]}")  # noqa: E501
+
             raise e.NodeCreationError(node_list, str(err))
 
         vapp.reload()
