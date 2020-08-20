064abbe6032c9df52e3d85276be39f04a8f55935
ltimothy <ltimothy@vmware.com>
2020-08-20 19:06:40 -0400

    added constants for mqtt consumer
    Signed-off-by: ltimothy <ltimothy@vmware.com>

--- container_service_extension/consumer/mqtt_consumer.py
+++ container_service_extension/consumer/mqtt_consumer.py
@@ -8,6 +8,12 @@ import paho.mqtt.client as mqtt
 import container_service_extension.consumer.utils as utils
 import container_service_extension.logger as logger
 
+BROKER_PATH = '/messaging/mqtt'
+CLIENT_ID = 'pythonMQTT'
+CONNECT_PORT = 443
+TRANSPORT_WSS = 'websockets'
+QOS_LEVEL = 2  # No duplicate messages
+
 
 class MQTTConsumer:
     def __init__(self,
@@ -29,7 +35,7 @@ class MQTTConsumer:
         def on_connect(client, userdata, flags, rc):
             logger.SERVER_LOGGER.info(f'MQTT client connected with result code'
                                       f' {rc} and flags {flags}')
-            client.subscribe(self.listen_topic, qos=2)
+            client.subscribe(self.listen_topic, qos=QOS_LEVEL)
 
         def on_message(client, userdata, msg):
             payload_json = json.loads(msg.payload.decode())
@@ -57,7 +63,7 @@ class MQTTConsumer:
 
             pub_ret = client.publish(topic=self.respond_topic,
                                      payload=json.dumps(response_json),
-                                     qos=2, retain=False)
+                                     qos=QOS_LEVEL, retain=False)
             logger.SERVER_LOGGER.info(f"pub_ret (rc, msg_id): {pub_ret}")
 
         def on_subscribe(client, userdata, msg_id, given_qos):
@@ -67,13 +73,13 @@ class MQTTConsumer:
         def on_disconnect(client, userdata, rc):
             logger.SERVER_LOGGER.info(f'MQTT disconnect with reason: {rc}')
 
-        client_id = 'PythonMQTT' # TODO: try other client id
-        mqtt_client = mqtt.Client(client_id=client_id, transport='websockets')
+        mqtt_client = mqtt.Client(client_id=CLIENT_ID,
+                                  transport=TRANSPORT_WSS)
         mqtt_client.username_pw_set(username=self.client_username,
                                     password=self.token)
         cert_req = ssl.CERT_REQUIRED if self.verify_ssl else ssl.CERT_NONE
         mqtt_client.tls_set(cert_reqs=cert_req)
-        mqtt_client.ws_set_options(headers={}, path='/messaging/mqtt')
+        mqtt_client.ws_set_options(path=BROKER_PATH)
 
         # Setup callbacks
         mqtt_client.on_connect = on_connect
@@ -82,7 +88,7 @@ class MQTTConsumer:
         mqtt_client.on_subscribe = on_subscribe
 
         try:
-            mqtt_client.connect(self.url, port=443)
+            mqtt_client.connect(self.url, port=CONNECT_PORT)
         except Exception as e:
             logger.SERVER_LOGGER.error(f'connection error: {e}')
             raise e
