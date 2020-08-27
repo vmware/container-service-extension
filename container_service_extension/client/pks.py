499a3c0c08dcc45e2e2eee449bec78889cbe4778
Aniruddha Shamasundar <aniruddha.9794@gmail.com>
2020-08-27 14:59:41 -0700

    enable pks ovdc commands for v35
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>

--- container_service_extension/client/pks.py
+++ container_service_extension/client/pks.py
@@ -8,7 +8,7 @@ import click
 from vcd_cli.utils import stderr
 from vcd_cli.utils import stdout
 
-from container_service_extension.client.legacy_ovdc import LegacyOvdc
+from container_service_extension.client.pks_ovdc import PksOvdc
 from container_service_extension.client.pks_cluster import PksCluster
 import container_service_extension.client.utils as client_utils
 from container_service_extension.logger import CLIENT_LOGGER
@@ -405,7 +405,7 @@ def list_ovdcs(ctx, list_pks_plans):
     try:
         client_utils.cse_restore_session(ctx)
         client = ctx.obj['client']
-        ovdc = LegacyOvdc(client)
+        ovdc = PksOvdc(client)
         result = ovdc.list_ovdc_for_k8s(list_pks_plans=list_pks_plans)
         stdout(result, ctx, sort_headers=False)
         CLIENT_LOGGER.debug(result)
@@ -448,7 +448,7 @@ def ovdc_enable(ctx, ovdc_name, pks_plan,
         client_utils.cse_restore_session(ctx)
         client = ctx.obj['client']
         if client.is_sysadmin():
-            ovdc = LegacyOvdc(client)
+            ovdc = PksOvdc(client)
             if org_name is None:
                 org_name = ctx.obj['profiles'].get('org_in_use')
             result = ovdc.update_ovdc_for_pks(
@@ -489,7 +489,7 @@ def ovdc_disable(ctx, ovdc_name, org_name):
         client_utils.cse_restore_session(ctx)
         client = ctx.obj['client']
         if client.is_sysadmin():
-            ovdc = LegacyOvdc(client)
+            ovdc = PksOvdc(client)
             if org_name is None:
                 org_name = ctx.obj['profiles'].get('org_in_use')
             result = ovdc.update_ovdc_for_pks(enable=False,
@@ -527,7 +527,7 @@ def ovdc_info(ctx, ovdc_name, org_name):
         client_utils.cse_restore_session(ctx)
         client = ctx.obj['client']
         if client.is_sysadmin():
-            ovdc = LegacyOvdc(client)
+            ovdc = PksOvdc(client)
             if org_name is None:
                 org_name = ctx.obj['profiles'].get('org_in_use')
             result = ovdc.info_ovdc_for_k8s(ovdc_name, org_name)
