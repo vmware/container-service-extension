ec27eb271ae6b725db0125b6665d7d2804ceaf32
Anirudh9794 <aniruddha.9794@gmail.com>
2020-08-28 10:32:44 -0700

    Changed name validation regex for cluster name (#715)
    * Changed name validation regex for cluster name
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>
    
    * Address review comments
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>
    
    * Added similar check to vcdbroker
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>

--- container_service_extension/def_/cluster_service.py
+++ container_service_extension/def_/cluster_service.py
@@ -1567,12 +1567,9 @@ def get_nfs_exports(sysadmin_client: vcd_client.Client, ip, vapp, vm_name):
 
 def is_valid_cluster_name(name):
     """Validate that the cluster name against the pattern."""
-    if len(name) > 25:
+    if name and len(name) > 25:
         return False
-    if name[-1] == '.':
-        name = name[:-1]
-    allowed = re.compile(r"(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
-    return all(allowed.match(x) for x in name.split("."))
+    return re.match("^[a-zA-Z][A-Za-z0-9-]*$", name) is not None
 
 
 def get_all_clusters(client, cluster_name=None, cluster_id=None,
