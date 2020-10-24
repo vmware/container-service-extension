fcc5b56e988f945765027f8083ec89cfdb0e77e9
Aniruddha Shamasundar <aniruddha.9794@gmail.com>
2020-10-23 17:12:51 -0700

    Modify de_cluster_native for native cluster apis
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>

--- container_service_extension/client/cse_client/cse_client.py
+++
@@ -1,7 +0,0 @@
-import pyvcloud.vcd.client as vcd_client
-import container_service_extension.shared_constants as shared_constants
-
-class CseClient:
-    def __init__(self, client: vcd_client.Client):
-        self._client = client
-        self._uri = f"{self._client.get_api_uri()}/{shared_constants.CSE_URL_FRAGMENT}/{shared_constants.CSE_3_0_URL_FRAGMENT}"
