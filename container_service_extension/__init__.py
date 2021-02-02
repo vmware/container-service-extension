992375de4379e734149e37e8fde1eb433d79e654
Sakthi Sundaram <sakthisunda@yahoo.com>
2021-02-02 14:55:09 -0800

    flake8 errrors
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>

--- container_service_extension/__init__.py
+++ container_service_extension/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2020 VMware, Inc. All Rights Reserved.
 # SPDX-License-Identifier: BSD-2-Claus
 
-from container_service_extension.logging.logger import configure_all_file_loggers
+from container_service_extension.logging.logger import configure_all_file_loggers  # noqa: E501
 from container_service_extension.logging.logger import configure_null_logger
 from container_service_extension.common.thread_local_data import init_thread_local_data  # noqa: E501
 
