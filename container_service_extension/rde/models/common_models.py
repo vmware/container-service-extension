2620f9d7369edb51811ed15f720a6654a48c26f7
Sahithi Ayloo <sahithi.ayloo@gmail.com>
2021-02-04 15:42:58 -0800

    Created RDE schema version 2.0.0 and the corresponding model class. Enhanced "Status" section of both model file and the schema with new "Cloud_properties" section.
    Signed-off-by: sahithi <sahithi.ayloo@gmail.com>

---
+++ container_service_extension/rde/models/common_models.py
@@ -0,0 +1,7 @@
+# container-service-extension
+# Copyright (c) 2021 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+
+# Placeholder for common models like DefInterface, Defined EntityType,
+# Defined Entity among others. These models need to be extracted out of
+# rde/models.py
