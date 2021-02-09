2405329e12036d397847a5c0c41836691fc53103
Sahithi Ayloo <sahithi.ayloo@gmail.com>
2021-02-09 13:26:09 -0800

    [VCDA-2051] Added RDE schema and model 2.0.0 and enhanced the status schema with new optional properties. (#903)
    * Created RDE schema version 2.0.0 and the corresponding model class. Enhanced "Status" section of both model file and the schema with new "Cloud_properties" section.

---
+++ container_service_extension/rde/models_/rde_1_0_0.py
@@ -0,0 +1,5 @@
+# container-service-extension
+# Copyright (c) 2021 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+
+# Placeholder for RDE model 1.0.0. It is currently residing under rde/models.py
