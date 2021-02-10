3c0032a226b48d067e1501ebb62ffaeff505ccff
Sahithi Ayloo <sahithi.ayloo@gmail.com>
2021-02-10 14:23:47 -0800

    Created packages for Converters, Validators, Backend. Moved the existing cluster_service.py to cluster_service_1_x.py Created cluster_service_2_x.py Implemented container_service_extension.rde.utils.get_cluster_service()
    Signed-off-by: sayloo <sahithi.ayloo@gmail.com>

--- container_service_extension/rde/utils.py
+++ container_service_extension/rde/utils.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
 """Utility methods to help interaction with defined entities framework."""
+import semantic_version
 
 import container_service_extension.exception.exceptions as excptn
 from container_service_extension.lib.cloudapi.cloudapi_client import CloudApiClient  # noqa: E501
@@ -60,3 +61,22 @@ def generate_entity_type_id(vendor, nss, version):
     :rtype str
     """
     return f"{def_constants.DEF_ENTITY_TYPE_ID_PREFIX}:{vendor}:{nss}:{version}"  # noqa: E501
+
+
+def get_cluster_service(rde_version_in_use, op_ctx):
+    """Get the right instance of backend cluster service.
+
+    Factory method to return the ClusterService based on the RDE version in use.
+    :param rde_version_in_use (str)
+    :param op_ctx (container_service_extension.security.context.operation_context.OperationContext)
+
+    :rtype cluster_service (container_service_extension.server.abstract_broker.AbstractBroker)  # noqa: E501
+    """
+    rde_version: semantic_version.Version = semantic_version.Version(rde_version_in_use)  # noqa: E501
+    if rde_version.major == 1:
+        from container_service_extension.rde.backend.cluster_service_1_x import ClusterService as ClusterService1X  # noqa: E501
+        return ClusterService1X(op_ctx=op_ctx)
+    elif rde_version.major == 2:
+        from container_service_extension.rde.backend.cluster_service_2_x import ClusterService as ClusterService2X  # noqa: E501
+        return ClusterService2X(op_ctx=op_ctx)
+
