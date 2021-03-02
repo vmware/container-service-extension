34c4fc38d867df4d76617e3478211a80fb5aab6f
Sakthi Sundaram <sakthisunda@yahoo.com>
2021-03-01 20:41:17 -0800

    validators by rde version
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>

--- container_service_extension/server/request_handlers/v36/def_cluster_handler.py
+++ container_service_extension/server/request_handlers/v36/def_cluster_handler.py
@@ -3,13 +3,17 @@
 # SPDX-License-Identifier: BSD-2-Clause
 from dataclasses import asdict
 
+import container_service_extension.common.constants.server_constants as server_constants  # noqa: E501
 from container_service_extension.common.constants.shared_constants import RequestKey  # noqa: E501
 import container_service_extension.common.utils.server_utils as server_utils
 import container_service_extension.lib.telemetry.constants as telemetry_constants  # noqa: E501
 import container_service_extension.lib.telemetry.telemetry_handler as telemetry_handler  # noqa: E501
 import container_service_extension.rde.backend.cluster_service_factory as cluster_service_factory  # noqa: E501
+import container_service_extension.rde.constants as rde_constants
 from container_service_extension.rde.models.abstractNativeEntity import AbstractNativeEntity  # noqa: E501
 import container_service_extension.rde.models.rde_factory as rde_factory
+from container_service_extension.rde.utils import construct_cluster_spec_from_entity_status  # noqa: E501
+import container_service_extension.rde.validators.validator_factory as rde_validator_factory  # noqa: E501
 import container_service_extension.security.context.operation_context as ctx
 import container_service_extension.server.request_handlers.request_utils as request_utils  # noqa: E501
 
@@ -33,12 +37,19 @@ def cluster_update(data: dict, op_ctx: ctx.OperationContext):
     svc = cluster_service_factory.ClusterServiceFactory(op_ctx). \
         get_cluster_service(rde_in_use)
     NativeEntityClass = rde_factory.get_rde_model(rde_in_use)
-    cluster_entity_spec: AbstractNativeEntity = \
+    cluster_entity: AbstractNativeEntity = \
         NativeEntityClass(**data[RequestKey.INPUT_SPEC])  # noqa: E501
     curr_entity = svc.entity_svc.get_entity(cluster_id)
-    is_upgrade_operation = \
-        request_utils.validate_cluster_update_request_and_check_cluster_upgrade(  # noqa: E501
-            asdict(cluster_entity_spec.spec), asdict(curr_entity.entity.spec))
+    cluster_entity_status = curr_entity.entity.status
+    current_entity_spec = construct_cluster_spec_from_entity_status(
+        cluster_entity_status, server_utils.get_rde_version_in_use())
+
+    is_upgrade_operation = rde_validator_factory.get_validator(
+        rde_constants.RuntimeRDEVersion.RDE_2_X).validate(
+        asdict(cluster_entity.spec),
+        asdict(current_entity_spec),
+        server_constants.CseOperation.V36_CLUSTER_UPDATE)
+
     if is_upgrade_operation:
-        return asdict(svc.upgrade_cluster(cluster_id, cluster_entity_spec))
-    return asdict(svc.resize_cluster(cluster_id, cluster_entity_spec))
+        return asdict(svc.upgrade_cluster(cluster_id, cluster_entity))
+    return asdict(svc.resize_cluster(cluster_id, cluster_entity))
