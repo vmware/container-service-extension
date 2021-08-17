cf1184f29f03865667f4bb605082730b92d8edb3
ltimothy <ltimothy@vmware.com>
2021-08-17 10:53:41 -0400

    removing is_tkgm boolean
    Signed-off-by: ltimothy <ltimothy@vmware.com>

---
+++ cluster_scripts/v2_x_tkgm/control_plane.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+set -e
+
+kubeadm_config_path=/root/kubeadm-defaults.conf
+vcloud_basic_auth_path=/root/vcloud-basic-auth.yaml
+vcloud_configmap_path=/root/vcloud-configmap.yaml
+vcloud_ccm_path=/root/cloud-director-ccm.yaml
+csi_driver_path=/root/csi-driver.yaml
+csi_controller_path=/root/csi-controller.yaml
+csi_node_path=/root/csi-node.yaml
+
+# tag images
+coredns_image_version=""
+etcd_image_version=""
+kubernetes_version=""
+declare -a tag_arr=("coredns" "etcd" "kube-proxy" "kube-apiserver" "kube-controller-manager" "kube-scheduler")
+for image in "${tag_arr[@]}"
+do
+  image_ref=$(ctr -n=k8s.io image list | cut -d" " -f1 | grep $image)
+  ref_path=$(echo $image_ref | sed 's/:.*//')
+  new_tag_version=$(echo $image_ref | sed 's/.*://' | sed 's/_/-/')
+  ctr -n=k8s.io image tag $image_ref $ref_path:$new_tag_version
+
+  # save image tags for later
+  if [ "$image" = "coredns" ]; then
+    coredns_image_version=new_tag_version
+  elif [ "$image" = "etcd" ]; then
+    etcd_image_version=new_tag_version
+  elif [ "$image" = "kube-proxy" ]; then # selecting other kube-* would work too
+    kubernetes_version=new_tag_version
+  fi
+done
+
+# create /root/kubeadm-defaults.conf
+echo "---
+apiVersion: kubeadm.k8s.io/v1beta2
+kind: InitConfiguration
+bootstrapTokens:
+- groups:
+  - system:bootstrappers:kubeadm:default-node-token
+  ttl: 0s
+  usages:
+  - signing
+  - authentication
+nodeRegistration:
+  criSocket: /run/containerd/containerd.sock
+  kubeletExtraArgs:
+    cloud-provider: external
+---
+apiVersion: kubeadm.k8s.io/v1beta2
+kind: ClusterConfiguration
+dns:
+  type: CoreDNS
+  imageRepository: projects.registry.vmware.com/tkg
+  imageTag: $coredns_image_version
+etcd:
+  local:
+    imageRepository: projects.registry.vmware.com/tkg
+    imageTag: $etcd_image_version
+networking:
+  serviceSubnet: {service_cidr}
+  podSubnet: {pod_cidr}
+imageRepository: projects.registry.vmware.com/tkg
+kubernetesVersion: $kubernetes_version
+---" > /root/kubeadm-defaults.conf
+kubeadm init --config $kubeadm_config_path > /root/kubeadm-init.out
+
+mkdir -p /root/.kube
+cp -f /etc/kubernetes/admin.conf /root/.kube/config
+chown $(id -u):$(id -g) /root/.kube/config
+export kubever=$(kubectl version --client | base64 | tr -d '\n')
+
+kubectl apply -f https://github.com/vmware-tanzu/antrea/releases/download/v0.11.3/antrea.yml
+systemctl restart kubelet
+while [ `systemctl is-active kubelet` != 'active' ]; do echo 'waiting for kubelet'; sleep 5; done
+
+# Download cpi and csi yaml
+#wget -O /root/vcloud-basic-auth.yaml https://raw.githubusercontent.com/vmware/cloud-provider-for-cloud-director/main/manifests/vcloud-basic-auth.yaml
+#wget -O /root/vcloud-configmap.yaml https://raw.githubusercontent.com/vmware/cloud-provider-for-cloud-director/main/manifests/vcloud-configmap.yaml
+#wget -O /root/cloud-director-ccm.yaml https://raw.githubusercontent.com/vmware/cloud-provider-for-cloud-director/main/manifests/cloud-director-ccm.yaml
+# TODO: change to use main branch links
+wget -O $vcloud_basic_auth_path https://raw.githubusercontent.com/ltimothy7/cloud-provider-for-cloud-director/auth_mount_internal/manifests/vcloud-basic-auth.yaml
+wget -O $vcloud_configmap_path https://raw.githubusercontent.com/ltimothy7/cloud-provider-for-cloud-director/auth_mount_internal/manifests/vcloud-configmap.yaml
+wget -O $vcloud_ccm_path https://raw.githubusercontent.com/ltimothy7/cloud-provider-for-cloud-director/auth_mount_internal/manifests/cloud-director-ccm.yaml
+wget -O $csi_driver_path https://github.com/vmware/cloud-director-named-disk-csi-driver/raw/main/manifests/csi-driver.yaml
+wget -O $csi_controller_path https://github.com/vmware/cloud-director-named-disk-csi-driver/raw/main/manifests/csi-controller.yaml
+wget -O $csi_node_path https://github.com/vmware/cloud-director-named-disk-csi-driver/raw/main/manifests/csi-node.yaml
+
+# TODO: look into if not https vcd host
+sed -i 's/BASE64_USERNAME/{base64_username}/; s/BASE64_PASSWORD/{base64_password}/' $vcloud_basic_auth_path
+sed -i 's/VCD_HOST/"https:\/\/{vcd_host}"/; s/ORG/"{org}"/; s/OVDC/"{ovdc}"/; s/NETWORK/"{ovdc_network}"/; s/VIP_SUBNET_CIDR/"{vip_subnet_cidr_ip}\/{vip_subnet_cidr_suffix}"/; s/CLUSTER_ID/"{cluster_id}"/' $vcloud_configmap_path
+
+kubectl apply -f $vcloud_basic_auth_path
+kubectl apply -f $vcloud_configmap_path
+kubectl apply -f $vcloud_ccm_path
+kubectl apply -f $csi_driver_path
+kubectl apply -f $csi_controller_path
+kubectl apply -f $csi_node_path
--- container_service_extension/rde/backend/cluster_service_factory.py
+++ container_service_extension/rde/backend/cluster_service_factory.py
@@ -4,9 +4,11 @@
 
 import semantic_version
 
+from container_service_extension.common.constants.shared_constants import ClusterEntityKind  # noqa: E501
 import container_service_extension.common.utils.server_utils as server_utils
 from container_service_extension.rde.backend.cluster_service_1_x import ClusterService as ClusterService1X  # noqa: E501
 from container_service_extension.rde.backend.cluster_service_2_x import ClusterService as ClusterService2X  # noqa: E501
+from container_service_extension.rde.backend.cluster_service_2_x_tkgm import ClusterService as ClusterService2XTKGm  # noqa: E501
 
 
 class ClusterServiceFactory:
@@ -28,4 +30,7 @@ class ClusterServiceFactory:
         if rde_version.major == 1:
             return ClusterService1X(op_ctx=self.req_ctx)
         elif rde_version.major == 2:
-            return ClusterService2X(self.req_ctx)
+            if self.req_ctx.entity.get('kind') == ClusterEntityKind.TKG_M.value:  # noqa: E501
+                return ClusterService2XTKGm(self.req_ctx)
+            else:
+                return ClusterService2X(self.req_ctx)
