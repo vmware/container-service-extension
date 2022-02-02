de60a7e974320faf501d2d5b62e7c34eb512e178
Andrew Ni <niandrew7@gmail.com>
2022-02-01 18:07:21 -0800

    Update cluster scripts to use containerd cri-socket for ubuntu 20.04
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- cluster_scripts/v2_x/node.sh
+++ cluster_scripts/v2_x/node.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-while [ `systemctl is-active docker` != 'active' ]
-do
-  echo 'waiting for docker'
-  sleep 5
-done
+while [ `systemctl is-active docker` != 'active' ]; do echo 'waiting for docker'; sleep 5; done
 
-kubeadm join {ip_port} --token {token} --discovery-token-ca-cert-hash {discovery_token_ca_cert_hash}
-
+lsb_release -a | grep -q 20.04
+if [ $? == 0 ]; then
+    # if os is ubuntu 20.04, then use containerd cri-socket
+    kubeadm join {ip_port} --token {token} --discovery-token-ca-cert-hash {discovery_token_ca_cert_hash} --cri-socket=/run/containerd/containerd.sock
+else
+    kubeadm join {ip_port} --token {token} --discovery-token-ca-cert-hash {discovery_token_ca_cert_hash}
+fi
