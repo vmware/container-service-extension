7b8722e4cc43cebfcf43a372ced0394ba4e5fbaa
sakthisvmware <43760465+sakthisvmware@users.noreply.github.com>
2022-02-15 15:07:14 -0800

    CSE Support-For-Syslog (#1302)
    * syslog support added
    
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>
    
    * rebase with latest master
    
    Signed-off-by: Sakthi Sundaram <sakthis@sakthis-a02.vmware.com>
    
    Co-authored-by: Sakthi Sundaram <sakthisunda@yahoo.com>

--- container_service_extension/server/service.py
+++ container_service_extension/server/service.py
@@ -308,6 +308,21 @@ class Service(object, metaclass=Singleton):
     def run(self, msg_update_callback=utils.NullPrinter()):
         sysadmin_client = None
         try:
+            syslog_host = None
+            syslog_port = None
+            try:
+                syslog_host = self.config.get_value_at('service.syslog_host')
+                syslog_port = self.config.get_value_at('service.syslog_port')
+            except KeyError:
+                pass
+            if syslog_host:
+                logger.configure_loggers_for_syslog(
+                    syslog_host=syslog_host,
+                    syslog_port=syslog_port
+                )
+        except Exception as err:
+            logger.SERVER_LOGGER.error(f"Syslog server error:{err}")
+        try:
             sysadmin_client = vcd_utils.get_sys_admin_client(api_version=None)
             verify_version_compatibility(
                 sysadmin_client,
