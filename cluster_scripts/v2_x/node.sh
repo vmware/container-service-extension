90cf8ebe09ae602983d3d5390151bddf3036ffef
Andrew Ni <niandrew7@gmail.com>
2022-02-16 02:38:51 -0800

    Move set -e command so script does not exit on exit code 1 for lsb_release command
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- cluster_scripts/v2_x/node.sh
+++ cluster_scripts/v2_x/node.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-set -e
-
 while [ `systemctl is-active docker` != 'active' ]; do echo 'waiting for docker'; sleep 5; done
 
 lsb_release -a | grep -q 20.04
 if [ $? == 0 ]; then
     # if os is ubuntu 20.04, then use containerd cri-socket
+    set -e
     kubeadm join {ip_port} --token {token} --discovery-token-ca-cert-hash {discovery_token_ca_cert_hash} --cri-socket=/run/containerd/containerd.sock
 else
+    set -e
     kubeadm join {ip_port} --token {token} --discovery-token-ca-cert-hash {discovery_token_ca_cert_hash}
 fi
