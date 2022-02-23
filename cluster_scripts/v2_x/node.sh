de01172b45fd2affe9fcc4966fc1e8d6eee6b816
Andrew Ni <niandrew7@gmail.com>
2022-02-23 09:38:41 -0800

    Move set -e command so script does not fail on lsb_release (#1309)

--- cluster_scripts/v2_x/node.sh
+++ cluster_scripts/v2_x/node.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 set -e
-
 while [ `systemctl is-active docker` != 'active' ]; do echo 'waiting for docker'; sleep 5; done
 
-lsb_release -a | grep -q 20.04
-if [ $? == 0 ]; then
+if [[ "$(lsb_release -rs)" =~ "20.04" ]]; then
     # if os is ubuntu 20.04, then use containerd cri-socket
     kubeadm join {ip_port} --token {token} --discovery-token-ca-cert-hash {discovery_token_ca_cert_hash} --cri-socket=/run/containerd/containerd.sock
 else
