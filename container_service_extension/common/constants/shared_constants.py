348b12bd1b3ae160c900f969765624c616a4aa68
ltimothy7 <66969084+ltimothy7@users.noreply.github.com>
2022-06-29 23:45:36 -0400

    [VCDA-3567] include list for cluster list (#1362)
    * include list for cluster list
    
    Signed-off-by: ltimothy7 <66969084+ltimothy7@users.noreply.github.com>
    
    * added all types of entities
    
    Signed-off-by: ltimothy7 <66969084+ltimothy7@users.noreply.github.com>
    
    * added include list entity id instructions
    
    Signed-off-by: ltimothy7 <66969084+ltimothy7@users.noreply.github.com>

--- container_service_extension/common/constants/shared_constants.py
+++ container_service_extension/common/constants/shared_constants.py
@@ -30,7 +30,10 @@ CSE_SERVER_LEGACY_MODE = "cse_server_running_in_legacy_mode"
 
 
 class EntityTypeId(Enum):
-    CAPVCD_1_0_0 = 'urn:vcloud:type:vmware:capvcdCluster:1.0.0'
+    NATIVE_1_0_0 = 'urn:vcloud:type:cse:nativeCluster:1.0.0'
+    NATIVE_2_0_0 = 'urn:vcloud:type:cse:nativeCluster:2.0.0'
+    NATIVE_2_1_0 = 'urn:vcloud:type:cse:nativeCluster:2.1.0'
+    TKG_1_0_0 = 'urn:vcloud:type:vmware:tkgcluster:1.0.0'
 
 
 class ClusterEntityKind(Enum):
